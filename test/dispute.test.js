a1a1065bd3ff3bad0210c8c960c24efc8e48e9e7
Isaac Frank <codingwithisaac@gmail.com>
2022-05-24 19:42:48 +0100

    Test Case Updated
    ** Refactor test script to match updated codebase

--- test/dispute.test.js
+++ test/dispute.test.js
@@ -44,7 +44,7 @@ describe("Dispute Flow", () => {
 
     await dispute
       .connect(server)
-      .createDisputeByServer(customer.address, merchant.address, 1, wei("2"), [
+      .createDisputeByServer(customer.address, merchant.address, 1, 20, [
         arbiter1.address,
         arbiter2.address,
         arbiter3.address,
@@ -81,32 +81,50 @@ describe("Dispute Flow", () => {
   it("finalizeDispute Function", async () => {
     const index = 0;
 
-    await expect(
-      dispute.connect(server).finalizeDispute(index, false, wei("1"))
-    ).to.be.revertedWith("ERC20: transfer amount exceeds balance");
+    await dispute.connect(server).finalizeDispute(index, false, wei("1"));
+
+    await expect(dispute.connect(server).claim(index)).to.be.revertedWith(
+      "ERC20: transfer amount exceeds balance"
+    );
 
     // Transfer funds to Dispute App
-    await mock.connect(deployer).transfer(dispute.address, wei("1"));
+    await mock.connect(deployer).transfer(dispute.address, wei("1000"));
 
-    const merchantOldBalance = await mock.balanceOf(merchant.address);
     const d = await dispute.getDisputeByIndex(index);
-    const rate = wei("0.000001");
-    const dollarValue = d.amount;
-    const received = dollarValue.div(rate);
-    // console.log(
-    //   "Rate:",
-    //   +rate,
-    //   "Dollar: $",
-    //   +dollarValue,
-    //   "Received: ",
-    //   +received
-    // );
-    await expect(dispute.connect(server).finalizeDispute(index, false, rate))
-      .to.emit(mock, "Transfer")
-      .withArgs(dispute.address, merchant.address, received);
+    console.log(d);
+    const dollarValue = d.usdValue;
+    const received = d.tokenValue;
+    const winner = d.winner;
 
-    const merchantNewBalance = await mock.balanceOf(merchant.address);
-    expect(merchantNewBalance).to.eq(merchantOldBalance + received);
+    console.log("Dollar: $", +dollarValue, "Received: ", +received);
+
+    if (winner == 1) {
+      const serverOldBalance = await mock.balanceOf(server.address);
+
+      await expect(dispute.connect(merchant).claim(index)).to.be.revertedWith(
+        "Only server can claim"
+      );
+
+      await expect(dispute.connect(server).claim(index))
+        .to.emit(mock, "Transfer")
+        .withArgs(dispute.address, server.address, received);
+
+      const serverNewBalance = await mock.balanceOf(server.address);
+      expect(serverNewBalance).to.eq(serverOldBalance + received);
+    } else if (winner == 2) {
+      const merchantOldBalance = await mock.balanceOf(merchant.address);
+
+      await expect(dispute.connect(server).claim(index)).to.be.revertedWith(
+        "Only sideB can claim"
+      );
+
+      await expect(dispute.connect(merchant).claim(index))
+        .to.emit(mock, "Transfer")
+        .withArgs(dispute.address, merchant.address, received);
+
+      const merchantNewBalance = await mock.balanceOf(merchant.address);
+      expect(merchantNewBalance).to.eq(merchantOldBalance + received);
+    }
 
     const deets = await dispute.getDisputeByIndex(index);
     expect(deets.state).to.eq(1);
@@ -120,13 +138,11 @@ describe("Dispute Flow", () => {
     for (let i = 0; i < 3; i++) {
       await dispute
         .connect(server)
-        .createDisputeByServer(
-          customer.address,
-          merchant.address,
-          1,
-          wei("2"),
-          [arbiter1.address, arbiter2.address, arbiter3.address]
-        );
+        .createDisputeByServer(customer.address, merchant.address, 1, 20, [
+          arbiter1.address,
+          arbiter2.address,
+          arbiter3.address,
+        ]);
     }
     const custOpen = await dispute.getCustomerOpenDisputes(customer.address);
     const custClosed = await dispute.getCustomerClosedDisputes(
