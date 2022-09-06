97283fb78c9d086e35bec3926400f046395683e4
Isaac Frank <codingwithisaac@gmail.com>
2022-09-06 18:41:02 +0100

    confirm that ratio-usd-token math is correct

---
+++ test/math.test.js
@@ -0,0 +1,173 @@
+const { ethers } = require("hardhat");
+const { expect } = require("chai");
+
+describe("Scenario Flow", () => {
+  let dispute;
+  let mock;
+  let erc721;
+  let first = true;
+  let [
+    deployer,
+    server,
+    customer,
+    merchant,
+    arbiter1,
+    arbiter2,
+    arbiter3,
+    arbiter4,
+  ] = Array(8).fill(null);
+  const votes = [{}];
+  const CHOICES = ["A", "B"];
+  const wei = ethers.utils.parseEther;
+
+  const tokenPerDollar = wei("0.5"); // 0.5 token == 1 dollar
+  const usdValue = 100e6; // 100 usd at stake
+
+  const makeChoice = (disputeIndex) => {
+    const choice = CHOICES[Math.floor(Math.random() * CHOICES.length)];
+    return `${disputeIndex}${choice}`;
+  };
+
+  beforeEach(async () => {
+    if (!first) return;
+    [
+      deployer,
+      server,
+      customer,
+      merchant,
+      arbiter1,
+      arbiter2,
+      arbiter3,
+      arbiter4,
+    ] = await ethers.getSigners();
+
+    const ERC20 = await ethers.getContractFactory("MockERC20");
+    const ERC721 = await ethers.getContractFactory("MockERC721");
+    const IARB = await ethers.getContractFactory("IterableArbiters");
+
+    erc721 = await ERC721.deploy("https://based.com/");
+    await erc721.safeMint(deployer.address, "");
+    await erc721.safeMint(deployer.address, "");
+    mock = await ERC20.deploy();
+
+    const args = [mock.address, server.address];
+    const DISPUTE = await ethers.getContractFactory("DisputeContract", {
+      libraries: {
+        IterableArbiters: (await IARB.deploy()).address,
+      },
+    });
+    dispute = await DISPUTE.deploy(...args);
+
+    first = false;
+  });
+
+  it("Server Creates Dispute [SUCCESS]", async () => {
+    let disputes = await dispute.getAllDisputes();
+    expect(disputes.length).to.eq(0);
+
+    await (
+      await dispute
+        .connect(server)
+        .createDisputeByServer(customer.address, merchant.address, true, erc721.address, 1, usdValue, [
+          arbiter1.address,
+          arbiter2.address,
+          arbiter3.address,
+          arbiter4.address,
+        ])
+    ).wait(1);
+
+    disputes = await dispute.getAllDisputes();
+
+    expect(disputes.length).to.eq(1);
+  });
+
+  it("Arbiters sign Votes", async () => {
+    const signers = [arbiter1, arbiter2, arbiter3, arbiter4];
+
+    for (let i = 0; i < signers.length; i++) {
+      const arbiterSigner = signers[i];
+
+      const choice = makeChoice(0);
+      const address = arbiterSigner.address;
+
+      const messageHash = ethers.utils.id(choice);
+      const messageHashBytes = ethers.utils.arrayify(messageHash);
+      const signature = await arbiterSigner.signMessage(messageHashBytes);
+
+      votes[i] = {
+        choice,
+        address,
+        signature,
+      };
+    }
+  });
+
+  it("Server submits signed votes without duplicates [SUCCESS] ", async () => {
+    const _msgs = [];
+    const _sigs = [];
+
+    for (let i = 0; i < 4; i++) {
+      _msgs.push(votes[i].choice);
+      _sigs.push(votes[i].signature);
+    }
+
+    let _dispute = await dispute.getDisputeByIndex(0);
+    expect(_dispute.voteCount).to.equal(0);
+
+    await (
+      await dispute.connect(server).castVotesWithSignatures(0, _sigs, _msgs)
+    ).wait(1);
+
+    _dispute = await dispute.getDisputeByIndex(0);
+    expect(_dispute.voteCount).to.equal(4);
+  });
+
+  it("Server should call finalizeDispute function [SUCCESS]", async () => {
+    await (
+      await dispute.connect(server).finalizeDispute(0, false, tokenPerDollar)
+    ).wait(1);
+
+    const _dispute = await dispute.getDisputeByIndex(0);
+
+    expect(_dispute.state).to.equal(1); // Closed
+  });
+
+  it("CLAIM: should ensure math is correct [Success]", async () => {
+    // Transfer funds to Dispute App
+    await mock.connect(deployer).transfer(dispute.address, wei("10000"));
+
+    const _dispute = await dispute.getDisputeByIndex(0);
+
+    let oldBalance = 0;
+    let newBalance = 0;
+
+    const tokenValue = (+usdValue * +tokenPerDollar) / 1e6;
+    expect(+_dispute.tokenValue).to.eq(tokenValue);
+    expect(tokenValue / 1e18).to.eq(50); // Confirm since we know the result before hand
+
+    if (_dispute.winner === 1) {
+      oldBalance = await mock.balanceOf(customer.address);
+      const tx = await dispute.connect(customer).claim(0);
+      expect(tx)
+        .to.emit(dispute, "DisputeFundClaimed")
+        .withArgs(_dispute.disputeIndex, tokenValue, customer.address);
+      expect(tx)
+        .to.emit(mock, "Transfer")
+        .withArgs(dispute.address, customer.address, tokenValue);
+
+      newBalance = await mock.balanceOf(customer.address);
+    } else if (_dispute.winner === 2) {
+      oldBalance = await mock.balanceOf(merchant.address);
+      const tx = await dispute.connect(merchant).claim(0);
+      expect(tx)
+        .to.emit(dispute, "DisputeFundClaimed")
+        .withArgs(_dispute.disputeIndex, tokenValue, merchant.address);
+      expect(tx)
+        .to.emit(mock, "Transfer")
+        .withArgs(dispute.address, merchant.address, tokenValue);
+
+      newBalance = await mock.balanceOf(merchant.address);
+    }
+    expect(+newBalance).to.eq(+oldBalance + +tokenValue);
+  });
+});
